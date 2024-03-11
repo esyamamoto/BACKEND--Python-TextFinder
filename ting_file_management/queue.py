c8234312f00eaf7648147ad0c5e65538c8c6b87d
esyamamoto <erika.s.yamamoto@gmail.com>
2024-03-11 15:36:36 -0300

    req1, comecando projetin

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,24 @@
+from operator import le
 from ting_file_management.abstract_queue import AbstractQueue
 
 
 class Queue(AbstractQueue):
-    def __init__(self):
-        """Inicialize sua estrutura aqui"""
+    def __init__(self):  # retorna uma fila vazia
+        self.data = []
 
-    def __len__(self):
-        """Aqui irá sua implementação"""
+    def __len__(self):  # retorna o tamanho da fila
+        return len(self.data)
 
-    def enqueue(self, value):
-        """Aqui irá sua implementação"""
+    def enqueue(self, value):  # add novo coisa no final da fila
+        self.data.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        # lista vazia?
+        if len(self) == 0:
+            raise IndexError("dequeue from empty queue")
+        return self.data.pop(0)  # remove e retona o primeiro coiso da fila
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= len(self.data):  # limite valido
+            raise IndexError("Índice Inválido ou Inexistente")
+        return self.data[index]
